45f910d6c32f5f0649957c3e34c11d79e409617d
Hugo Falci <168432476+HugoFalci@users.noreply.github.com>
2024-06-10 21:39:28 -0300

    Lado esquero 80%

---
+++ src/App copy.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+    <!-- Container principal, usando classes de layout do Vuetify para centralizar vertical e horizontalmente -->
+    <div class="d-flex justify-center align-center" style="height: 100vh;">
+      <!-- Imagem de fundo que preenche todo o espaço disponível -->
+      <img src="./assets/imagens/background.jpg" class="absolute-fill">
+      <!-- Overlay sobre a imagem de fundo -->
+      <div class="overlay d-flex half">
+        <!-- Divisão da tela em duas metades -->
+          <v-col cols="7">
+            <div class="d-flex flex-column align-center">
+              <spam class="d-flex flex-column mb-6 mx-12">
+                <h1 class="d-flex flex-column align-center text--#E94E24">
+                  <u>PARE DE DESPERDIÇAR </u>
+                  <p>
+                    <u>DINHEIRO </u>e maximize
+                  </p>
+                  <u>seus lucros</u>
+                </h1>
+              </spam>
+              <p>
+                <spam>Calcule sua redução de custos na gestão financeira de locações de imóveis com a plataforma Imobia
+                </spam>
+              </p>
+              <strong>Reduzir seus custos operacionais e aumentar seu lucro bruto é seu objetivo?</strong>
+              <div>
+                <p>As empresas do mercado imobiliário estão buscando cada vez mais eficiência para organizar sua rotina
+                  de cobranças e pagamentos de aluguéis.
+                  Somos motivados a ajudar você a fazer uma gestão financeira de um jeito fácil e automático, vem com a
+                  gente!</p>
+              </div>
+            </div>
+          </v-col>
+          <v-col cols="6">6</v-col>
+      </div>
+    </div>
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+//
+</script>
+
+<style scoped>
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.half {
+  background-color: rgba(0, 0, 0, 0.5);
+  /* Cor de fundo semi-transparente */
+  color: white;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,37 @@
 <template>
   <v-app>
-    <!-- Container principal, usando classes de layout do Vuetify para centralizar vertical e horizontalmente -->
-    <div class="d-flex justify-center align-center" style="height: 100vh;">
-      <!-- Imagem de fundo que preenche todo o espaço disponível -->
-      <img src="./assets/imagens/background.jpg" class="absolute-fill">
-      <!-- Overlay sobre a imagem de fundo -->
-      <div class="overlay d-flex half">
-        <!-- Divisão da tela em duas metades -->
-          <v-col cols="7">
-            <div class="d-flex flex-column align-center">
-              <spam>
-                <h1 class="tittle d-flex flex-column align-center">
-                  <u>PARE DE DESPERDIÇAR</u>
-                  <u>DINHEIRO </u>
-                  e maximize
-                  <br>
-                  <u>seus lucros</u>
+    <v-img src="./assets/imagens/background.jpg" cover>
+      <!-- Adicionando uma camada fosca com um div -->
+      <div class="frosted-layer"></div>
+        <v-container>
+          <v-row>
+            <v-col cols="6" align-self="center" class="pa-10 pt-16">
+              <v-card flat class="bg-transparent mt-16 text-center">
+                <h1 class="titulo text-tituloLaranja font-weight-bold">
+                  <spam class="underline-uppercase">PARE DE DESPERDIÇAR </spam>
+                  <spam class="underline-uppercase">DINHEIRO</spam> e maximize
+                  <spam class="underline-uppercase">seus lucros</spam>
                 </h1>
-              </spam>
-              <p>
-                <spam>Calcule sua redução de custos na gestão financeira de locações de imóveis com a plataforma Imobia
-                </spam>
-              </p>
-              <strong>Reduzir seus custos operacionais e aumentar seu lucro bruto é seu objetivo?</strong>
-              <div>
-                <p>As empresas do mercado imobiliário estão buscando cada vez mais eficiência para organizar sua rotina
-                  de cobranças e pagamentos de aluguéis.
-                  Somos motivados a ajudar você a fazer uma gestão financeira de um jeito fácil e automático, vem com a
-                  gente!</p>
-              </div>
-            </div>
-          </v-col>
-          <v-col cols="6">6</v-col>
-      </div>
-    </div>
+
+                <h2 class="subtitulo_caucule_reducao text-tituloLaranja text-center font-weight-bold mt-8">
+                  Calcule sua redução de custos na gestão financeira de locações de imóveis com a
+                  plataforma Imobia
+                </h2>
+
+                <div class="conteudo text-white pa-3 pt-8">
+                  <b>Reduzir seus custos operacionais e aumentar seu lucro bruto é seu objetivo?</b>
+                  <br><br>
+                  As empresas do mercado imobiliário estão buscando cada vez mais eficiência 
+                  para organizar sua rotina de cobranças e pagamentos de aluguéis. 
+                  Somos motivados a ajudar você a fazer uma gestão financeira de um jeito fácil e automático, 
+                  vem com a gente!
+                </div>
+              </v-card>
+            </v-col>
+            <v-col cols="6">1</v-col>
+          </v-row>
+        </v-container>
+    </v-img>
     <AppFooter />
   </v-app>
 </template>
@@ -43,24 +41,31 @@
 </script>
 
 <style scoped>
-.overlay {
+.titulo {
+  font-size: 45px;
+}
+
+.subtitulo_caucule_reducao {
+  font-size: 30px;
+  letter-spacing: 2px;
+}
+
+.conteudo {
+  font-size: 22px;
+}
+
+.frosted-layer {
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
+  background-color: rgb(30, 50, 96, 0.7);
+  /* Cor branca com opacidade de 50% */
 }
 
-.half {
-  background-color: rgba(0, 0, 0, 0.5);
-  /* Cor de fundo semi-transparente */
-  color: white;
-}
-
-.tittle{
-  color: #E94E24;
-  font-size: 41px;
-  font-family: Coda, display;
-  text-align: center;
+.underline-uppercase {
+  display: inline-block;
+  text-decoration: underline;
 }
 </style>
